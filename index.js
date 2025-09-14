789e0a9a6a8e396ef4d618748df803a94bbdaeb2
Pouria <108181964+pouriavj@users.noreply.github.com>
2025-09-14 22:28:59 +0330

    Initial Commit

---
+++ index.js
@@ -0,0 +1,51 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+app.get("/", async (req, res) => {
+  try {
+    const response = await axios.get("https://bored-api.appbrewery.com/random");
+    const result = response.data;
+    res.render("index.ejs", { data: result });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
+});
+let errorMessage;
+app.post("/", async (req, res) => {
+  console.log(req.body);
+
+ 
+  
+    try {
+    const response = await axios.get(`https://bored-api.appbrewery.com/filter?type=${req.body.type}&participants=${req.body.participants}`);
+    const result = response.data;
+    res.render("index.ejs", { data: result[Math.floor(Math.random()*result.length)] });
+  } catch (error) {
+    if (error.response.status === 404){
+        errorMessage = "No activities that match your criteria.";
+    }
+    else {
+      errorMessage = error.message;
+    }
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: errorMessage,
+    });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
